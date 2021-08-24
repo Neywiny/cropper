20b4c2ac16e14035aa1cdb9122b79d1b4b66ebdb
Dylan Shekter <dylan@shekter.dev>
2021-08-24 17:30:25 -0400

    added cancel and dashed box

--- cropper.py
+++ cropper.py
@@ -24,10 +24,10 @@ def mouse_down(event):
 	elif event.y <= 10:
 		event.y = 0
 	if state == 0:
-		x1.set(event.x)
-		y1.set(event.y)
+		x1_var.set(event.x)
+		y1_var.set(event.y)
 	elif state == 1:
-		box = (x1.get()*scale, y1.get()*scale, event.x*scale, event.y*scale)
+		box = (x1_var.get()*scale, y1_var.get()*scale, event.x*scale, event.y*scale)
 		ic = img.crop(box)
 		idx = img_index.get()
 		ic.save('done/' + images[idx])
@@ -45,6 +45,18 @@ def mouse_down(event):
 		
 	state_var.set(state + 1)
 	#print(event.x, event.y)
+	
+def motion(event):
+	if state_var.get() == 1:
+		x2, y2 = event.x, event.y
+		x1, y1 = x1_var.get(), y1_var.get()
+		canvas.coords(rect, x1, y1, x2, y1, x2, y2, x1, y2, x1, y1)
+		canvas.itemconfigure(rect, state=NORMAL)
+	else:
+		canvas.itemconfigure(rect, state=HIDDEN)
+def escape(event):
+	state_var.set(0)
+	canvas.itemconfigure(rect, state=HIDDEN)
 
 images = os.listdir("raw")
 images.sort()
@@ -52,18 +64,23 @@ images.sort()
 root = Tk()
 root.title('cropper')
 state_var = IntVar(root)
-x1 = IntVar(root)
-y1 = IntVar(root)
+x1_var = IntVar(root)
+y1_var = IntVar(root)
 img_index = IntVar(root)
 
 scale = 9
 canvas = Canvas(root, height=100, width=100)
 canvas.pack() 
 
+
+
 img = Image.open('raw/' + images[img_index.get()])
 resizer(root, img, canvas)
 p_img = ImageTk.PhotoImage(img.resize((img.size[0]//scale, img.size[1]//scale)))
 c_img = canvas.create_image(0, 0, anchor=NW, image=p_img)
+rect = canvas.create_line(0, 0, 0, 0, 0, 0, 0, 0, dash=(1, 1), activewidth=3, disabledwidth=0, state=HIDDEN)
 canvas.bind("<1>", mouse_down)
+canvas.bind('<Motion>', motion)
+root.bind('<Escape>', escape)
 
 root.mainloop()
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 takes photos in done/ and puts them in raw/
 
 So have those folders be made beforehand
+
+press escape to cancel the current selection
